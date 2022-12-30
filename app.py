65ad7de0accd23100bbde009f6fc1a0a43d49f6e
Jessica Hsiang <hsiang.jessica@gmail.com>
2022-12-30 11:40:45 -0800

    Changed question_page/handle_question views order and removed comment

--- app.py
+++ app.py
@@ -18,21 +18,18 @@ def start_page():
 
   return render_template('start.html', title=title, instructions=instructions)
 
-
-@app.route('/questions/<int:question_id>', methods = ['POST'])
-def question_page(question_id):
-  question = survey.questions[question_id]
-  choices = survey.questions[question_id].choices
-  return render_template('questions.html', question=question,question_num=question_id, choices=choices)
-
 @app.route('/answer', methods = ['POST'])
 def handle_question():
   """Save answer to list and redirect to next question."""
   answer = request.form['answer']
   responses.append(answer)
-  import pdb
-  pdb.set_trace()
   return redirect(f'/questions/{len(responses)}') 
-  #not working - method not allowed. tried redirecting to homepage. tried using pdb debugger.
+  
+@app.route('/questions/<int:question_id>', methods = ['POST'])
+def question_page(question_id):
+  """Display current question."""
+  question = survey.questions[question_id]
+  choices = survey.questions[question_id].choices
+  return render_template('questions.html', question=question, choices=choices)
 
   
