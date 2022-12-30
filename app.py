23a0ecadfce64dd8d9189cb1cec31f39683e62c9
Jessica Hsiang <hsiang.jessica@gmail.com>
2022-12-30 13:54:44 -0800

    Added flash, updated GET/POST, and conditionals

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -13,6 +13,7 @@ responses = []
 @app.route('/')
 def start_page():
   """Show start page with survey title and instructions"""
+  responses.clear()
   title = survey.title
   instructions = survey.instructions
 
@@ -29,15 +30,27 @@ def handle_question():
   else:
     return redirect(f'/questions/{len(responses)}')
   
-@app.route('/questions/<int:question_id>', methods = ['POST'])
-def question_page(question_id):
+@app.route('/questions/<int:question_id>')
+def show_question(question_id):
   """Display current question."""
   question = survey.questions[question_id]
   choices = survey.questions[question_id].choices
+
+  if(responses is False):
+    return redirect('/')
+  
+  if(len(responses) == len(survey.questions)):
+    return redirect('/complete')
+  
+  if(question_id != len(responses)):
+    flash(f'Invalid question id: {question_id}')
+    return redirect(f'/questions/{len(responses)}')
+
   return render_template('questions.html', question=question, question_id=question_id, choices=choices)
 
 @app.route('/complete')
 def complete():
+  """Survey complete. Show completion page."""
   return render_template('complete.html')
 
   
